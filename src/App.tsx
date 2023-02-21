f86bcbadb5178e008b774d0c30134720d31e8dfa
Ryota Konabe <rt.konabe@gmail.com>
2023-02-22 01:30:45 +0900

    add mueller lyer illusion experiment

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,90 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import MuellerLyerDiagram from "./components/MuellerLyerDiagram";
 
 function App() {
+  const [trial, setTrial] = useState(0);
+  const [inputData, setInputData] = useState<{ top: number; bottom: number }[]>(
+    []
+  );
+  const [outputData, setOutputData] = useState<string[]>([]);
+  useEffect(() => {
+    const data = Array(20)
+      .fill({})
+      .map(() => {
+        return {
+          top: 150 + Math.random() * 30 - 60,
+          bottom: 150 + Math.random() * 30 - 60,
+        };
+      });
+    setInputData(data);
+  }, []);
+
+  const onKeyPress = (e: React.KeyboardEvent) => {
+    const key = e.code;
+    if (trial === 20) {
+      return;
+    }
+    if (key === "ArrowUp") {
+      setOutputData([...outputData, "top"]);
+      setTrial(trial + 1);
+    }
+    if (key === "ArrowDown") {
+      setOutputData([...outputData, "bottom"]);
+      setTrial(trial + 1);
+    }
+  };
+
+  const Stimulus = ({
+    trialData,
+  }: {
+    trialData: { top: number; bottom: number } | undefined;
+  }) => {
+    {
+      return trialData != null ? (
+        <div>
+          {trial}
+          <MuellerLyerDiagram
+            width={300}
+            height={100}
+            isOpen={true}
+            angle={30}
+            barLength={trialData.top}
+            arrowLength={50}
+          />
+          <MuellerLyerDiagram
+            width={300}
+            height={100}
+            isOpen={false}
+            angle={30}
+            barLength={trialData.bottom}
+            arrowLength={50}
+          />
+        </div>
+      ) : (
+        <div>
+          正答率
+          {outputData.filter((v, i) => {
+            const data = inputData[i];
+            return data.top > data.bottom && v === "top";
+          }).length / 20.0}
+          %
+        </div>
+      );
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header>オープン心理学実験</header>
+      <div
+        tabIndex={0}
+        style={{ outline: "none", width: "100%" }}
+        onKeyDown={onKeyPress}
+      >
+        <h1>Müller-Lyer illusion</h1>
+        <p>上の方が長く見えたら↑キーを、下のほうが長く見えたら↓キーを</p>
+        <Stimulus trialData={inputData[trial]} />
+      </div>
     </div>
   );
 }
---
+++ src/components/MuellerLyerDiagram.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import { Layer, Line, Stage } from "react-konva";
+
+type MuellerLyerDiagramProps = {
+  width: number;
+  height: number;
+  isOpen: boolean;
+  angle: number;
+  barLength: number;
+  arrowLength: number;
+};
+
+const radianToDegree = (degree: number) => degree * (Math.PI / 180);
+
+function MuellerLyerDiagram({
+  width,
+  height,
+  isOpen,
+  barLength,
+  arrowLength,
+  angle,
+}: MuellerLyerDiagramProps) {
+  const radian = radianToDegree(angle);
+
+  const center = { x: width / 2, y: height / 2 };
+  const intersectLeft = { x: center.x - barLength / 2.0, y: center.y };
+  const intersectRight = { x: center.x + barLength / 2.0, y: center.y };
+
+  const LeftArrow = ({ isOpen }: { isOpen: boolean }) => (
+    <Line
+      points={[
+        intersectLeft.x + (isOpen ? -1 : 1) * arrowLength * Math.cos(radian),
+        intersectLeft.y - arrowLength * Math.sin(radian),
+        intersectLeft.x,
+        intersectLeft.y,
+        intersectLeft.x + (isOpen ? -1 : 1) * arrowLength * Math.cos(radian),
+        intersectLeft.y + arrowLength * Math.sin(radian),
+      ]}
+      stroke={"black"}
+    />
+  );
+
+  const bar = (
+    <Line
+      points={[
+        intersectLeft.x,
+        intersectLeft.y,
+        intersectRight.x,
+        intersectRight.y,
+      ]}
+      stroke={"black"}
+    />
+  );
+
+  const RightArrow = ({ isOpen }: { isOpen: boolean }) => (
+    <Line
+      points={[
+        intersectRight.x + (isOpen ? 1 : -1) * arrowLength * Math.cos(radian),
+        intersectRight.y - arrowLength * Math.sin(radian),
+        intersectRight.x,
+        intersectRight.y,
+        intersectRight.x + (isOpen ? 1 : -1) * arrowLength * Math.cos(radian),
+        intersectRight.y + arrowLength * Math.sin(radian),
+      ]}
+      stroke={"black"}
+    />
+  );
+
+  return (
+    <div style={{ width, height }}>
+      <Stage width={width} height={height}>
+        <Layer>
+          <LeftArrow isOpen={isOpen} />
+          {bar}
+          <RightArrow isOpen={isOpen} />
+        </Layer>
+      </Stage>
+    </div>
+  );
+}
+
+export default MuellerLyerDiagram;
